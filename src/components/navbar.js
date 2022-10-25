e2bca0b21c0533a16d15b94f23e37fe2c8ca2e36
Ved Patel <masteransh02@gmail.com>
2022-10-25 21:31:28 +0530

    initial commit

---
+++ src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+function Greet(props){
+    return (//<div id="check"><h2>I am a { props.val }  {props.ved}!</h2>
+    //</div>
+    <section id="navbar">
+      <div class="bar">
+        <img class="logo" src="./images/logo3.png" alt=""/>
+        <div class="links">
+              <a class="link" href="#">Home </a>
+              <a class="link" href="#">About Us </a>
+              <a class="link" href="#">Blog </a>
+              <a class="link" href="#">Contact </a>
+        </div>
+        
+      </div>
+    </section>
+
+    );
+  }
+export default Greet
+
---
+++ src/components/top.js
@@ -0,0 +1,21 @@
+import React from "react";
+function Top(props){
+    return (//<div id="check"><h2>I am a { props.val }  {props.ved}!</h2>
+    //</div>
+    <section id="top">
+      <div class="tagline">
+        <div class="titlebox">
+            <div class="cname">prafal agro</div>
+            <div class="tag">Empowering &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Rural Economy Through Mushroom Cultivation</div>
+            <div class="learn">Learn more</div>
+        </div>
+      </div>
+        <div class="headimg">
+            <img class="headimg" src="./images/mush2.jpg" alt=""/>
+        </div>
+    </section>
+
+    );
+  }
+export default Top
+
